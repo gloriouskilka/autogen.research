c7bd32b0fe1d44534e5b7d7db8b632cd97e70f75
Ilya Shutov <shutovilya@gmail.com>
2024-12-18 09:26:43 +0100

    upd

--- combine_files.py
+++ combine_files.py
@@ -77,7 +77,7 @@ def main():
     exclude_dirs = [".git", ".github"]
 
     # part = ["agents", "base", "conditions", "state", "task", "teams", "ui"]
-    part = ["_autogen.projectfiles", "agents", "test_utils", "tests", "tools", "books_sql"]
+    part = ["_autogen.projectfiles", "agents", "books_sql", "test_utils", "tests", "tools"]
     include_top_level_dirs = part
 
     folders = list_top_level_folders(args.directory, include_top_level_dirs, exclude_dirs)
